512946e6dd4acca8f230bb8e382ee77f35de2021
Ostiary <ec1o.force@gmail.com>
2022-02-11 20:18:33 +0200

    Добавьте файлы проекта.

---
+++ 006_Char/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Char
+{
+    class Program
+    {
+        static void Main()
+        {
+            char a = 'A';      // Символ
+            char b = '\x3D'; // Значение в 16-ричном формате '\x0039' - также можно
+            char c = '\u0039'; // Значение в формате unicode
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string n = "\nмоя строка 1";
+			string t = "\tмоя строка 2";
+			string a = "\aмоя строка 3";
+		}
+	}
+}
